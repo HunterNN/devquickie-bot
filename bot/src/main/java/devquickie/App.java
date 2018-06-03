9797396273065de6f9093125f3234463aefe75a1
Hunter99 <stephan.kessler@outlook.com>
2018-06-03 20:22:16 +0200

    added jda login

--- bot/src/main/java/devquickie/App.java
+++ bot/src/main/java/devquickie/App.java
@@ -1,9 +1,59 @@
 package devquickie;
 
-public class App 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+public class App implements EventListener
 {
+    private static final String token_path = "bot/assets/token.txt";
+
+    private static String token;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        loadToken();
+        try {
+            JDA jda = new JDABuilder(AccountType.BOT)
+                .setToken(token)
+                .addEventListener(new App())
+                .buildBlocking();
+
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
+    
+    private static void loadToken(){
+        File file = new File(token_path);
+    
+        try {
+            Scanner sc = new Scanner(file);
+            token = sc.nextLine();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
+        }    
+    }
+
+	@Override
+	public void onEvent(Event event) {
+		if(event instanceof ReadyEvent){
+            System.out.println("hat geklappt");
+        }
+	}
 }
